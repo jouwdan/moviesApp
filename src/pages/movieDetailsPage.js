9f94d9c256d656109559ec3c829ef47a8bef2c24
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-06-17 18:22:45 +0100

    Movie Details Page rendering.

---
+++ src/pages/movieDetailsPage.js
@@ -0,0 +1,66 @@
+import React from "react";
+import MovieHeader from "../components/headerMovie/";
+import MovieDetails from "../components/movieDetails/";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import ImageList from "@material-ui/core/ImageList";
+import ImageListItem from "@material-ui/core/ImageListItem";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
+  gridList: {
+    width: 450,
+    height: "100vh",
+  },
+}));
+
+const MoviePage = (props) => {
+  const classes = useStyles();
+  const movie = props.movie;
+  const images = props.images;
+
+  return (
+    <>
+      {movie ? (
+        <>
+          <MovieHeader movie={movie} />
+          <Grid container spacing={5} style={{ padding: "15px" }}>
+            <Grid item xs={3}>
+              <div className={classes.root}>
+                <ImageList
+                  rowHeight={500}
+                  className={classes.gridList}
+                  cols={1}
+                >
+                  {images.map((image) => (
+                    <ImageListItem
+                      key={image.file_path}
+                      className={classes.gridListTile}
+                      cols={1}
+                    >
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500/${image}`}
+                        alt={image.poster_path}
+                      />
+                    </ImageListItem>
+                  ))}
+                </ImageList>
+              </div>
+            </Grid>
+            <Grid item xs={9}>
+              <MovieDetails movie={movie} />
+            </Grid>
+          </Grid>
+        </>
+      ) : (
+        <h2>Waiting for API data</h2>
+      )}
+    </>
+  );
+};
+
+export default MoviePage;
