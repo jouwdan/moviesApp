4e1d5c81d4953c9bfcbc0c269404e4a1c790c247
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-06-17 18:55:25 +0100

    Show movie review excerpts; Refactor Details page.

---
+++ src/components/templateMoviePage/index.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import MovieHeader from "../headerMovie";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import ImageList from "@material-ui/core/ImageList";
+import ImageListItem from "@material-ui/core/ImageListItem";
+import { getMovieImages } from "../../api/tmdb-api";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(7),
+  },
+  imageListRoot: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
+  imageList: {
+    width: 450,
+    height: '100vh',
+  },
+}));
+
+const TemplateMoviePage = ({ movie, children }) => {
+  const classes = useStyles();
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    getMovieImages(movie.id).then((images) => {
+      setImages(images);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <MovieHeader movie={movie} />
+
+      <Grid container spacing={5} style={{ padding: "15px" }}>
+        <Grid item xs={3}>
+          <div className={classes.imageListRoot}>
+            <ImageList rowHeight={500} className={classes.gridList} cols={1}>
+              {images.map((image) => (
+                <ImageListItem key={image.file_path} cols={1}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                    alt={image.poster_path}
+                  />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </div>
+        </Grid>
+
+        <Grid item xs={9}>
+          {children}
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default TemplateMoviePage;
--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,30 +1,12 @@
-import React, {useState, useEffect}  from "react";
-import { getMovie, getMovieImages } from "../api/tmdb-api";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import MovieHeader from "../components/headerMovie/";
-import MovieDetails from "../components/movieDetails/";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
-import ImageList from "@material-ui/core/ImageList";
-import ImageListItem from "@material-ui/core/ImageListItem";
+import MovieDetails from "../components/movieDetails";
+import PageTemplate from "../components/templateMoviePage";
+import { getMovie } from "../api/tmdb-api";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-  },
-  gridList: {
-    width: 450,
-    height: "100vh",
-  },
-}));
-
-const MoviePage = (props) => {
-  const classes = useStyles();
+const MovieDetailsPage = (props) => {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
-  const [images, setImages] = useState([]);
 
   useEffect(() => {
     getMovie(id).then((movie) => {
@@ -32,51 +14,19 @@ const MoviePage = (props) => {
     });
   }, [id]);
 
-  useEffect(() => {
-    getMovieImages(id).then((images) => {
-      setImages(images);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <>
       {movie ? (
         <>
-          <MovieHeader movie={movie} />
-          <Grid container spacing={5} style={{ padding: "15px" }}>
-            <Grid item xs={3}>
-              <div className={classes.root}>
-                <ImageList
-                  rowHeight={500}
-                  className={classes.gridList}
-                  cols={1}
-                >
-                  {images.map((image) => (
-                    <ImageListItem
-                      key={image.file_path}
-                      className={classes.gridListTile}
-                      cols={1}
-                    >
-                      <img
-              src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-              alt={image.file_path}
-          />
-                    </ImageListItem>
-                  ))}
-                </ImageList>
-              </div>
-            </Grid>
-            <Grid item xs={9}>
-              <MovieDetails movie={movie} />
-            </Grid>
-          </Grid>
+          <PageTemplate movie={movie}>
+            <MovieDetails movie={movie} />
+          </PageTemplate>
         </>
       ) : (
-        <h2>Waiting for API data</h2>
+        <p>Waiting for movie details</p>
       )}
     </>
   );
 };
 
-export default MoviePage;
+export default MovieDetailsPage;
